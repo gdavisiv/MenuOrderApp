95a23d5b9fcd82ed1af842de66ce79bc89b125a0
gdavisiv <gdavisiv@gmail.com>
2020-03-08 13:41:04 -0400

    Adding code to save data/persistence

--- MenuOrders/ContentView.swift
+++ MenuOrders/ContentView.swift
@@ -10,6 +10,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    //Unfortunately there’s a bug in the current version of SwiftUI. Since we injected into the SceneDelegate’s
+    //scene function, the managed object context should be accessible globally by all views by using the
+    //corresponding environment property. However, doing this inside popover views like our OrderSheet won’t work
+    //properly. What we have to do is to pass the managedObjectContext that gets initialised inside the scene
+    //function downwards to our OrderSheet. Thus, we have to use the @Environment property inside our ContentView,
+    //too …
+    @Environment(\.managedObjectContext) var mangedObjectContext
     @State var showOrderSheet = false
     var body: some View {
         NavigationView {
@@ -23,8 +30,8 @@ struct ContentView: View {
                     .resizable()
                     .frame(width: 32, height: 32, alignment: .center)
             }))
-                .sheet(isPresented: $showOrderSheet){
-            OrderSheet()
+                .sheet(isPresented: $showOrderSheet) {
+                    OrderSheet().environment(\.managedObjectContext, self.managedObjectContext)
             }
         }
     }
--- MenuOrders/OrderSheet.swift
+++ MenuOrders/OrderSheet.swift
@@ -10,6 +10,13 @@
 import SwiftUI
 
 struct OrderSheet: View {
+    //Unfortunately there’s a bug in the current version of SwiftUI. Since we injected into the SceneDelegate’s
+    //scene function, the managed object context should be accessible globally by all views by using the
+    //corresponding environment property. However, doing this inside popover views like our OrderSheet won’t work
+    //properly. What we have to do is to pass the managedObjectContext that gets initialised inside the scene
+    //function downwards to our OrderSheet. Thus, we have to use the @Environment property inside our ContentView,
+    //too …
+    @Environment(\.managedObjectContext) var managedObjectContext
     
     let pizzaTypes = ["G", "J", "Liz", "Chica"]
     
